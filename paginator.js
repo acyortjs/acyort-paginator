e24b0cd5e417667d82906953b6bdaa23faa6cf1e
LoeiFy <LoeiFy@gmail.com>
2018-01-15 16:54:31 +0800

    lint fixed

--- paginator.js
+++ paginator.js
@@ -35,20 +35,20 @@ function paginator({ base, current, total }, more = '&hellip;') {
     '</span>',
   ]
 
-  if (total < step * 2 + 6) {
+  if (total < (step * 2) + 6) {
     html[1] = num(1, total + 1)
     return html.join('')
   }
 
-  if (current < step * 2 + 1) {
-    html[1] += num(1, step * 2 + 4)
+  if (current < (step * 2) + 1) {
+    html[1] += num(1, (step * 2) + 4)
     html[1] += last()
     return html.join('')
   }
 
-  if (current > total - step * 2) {
+  if (current > total - (step * 2)) {
     html[1] += first()
-    html[1] += num(total - step * 2 - 2, total + 1)
+    html[1] += num(total - (step * 2) - 2, total + 1)
     return html.join('')
   }
 
